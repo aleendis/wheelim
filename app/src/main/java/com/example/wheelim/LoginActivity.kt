5406a0006dd03a164a138c3161f90c97cd38b468
aleendis <43637273+aleendis@users.noreply.github.com>
2023-05-23 03:11:28 +0900

    activity

--- app/src/main/java/com/example/wheelim/LoginActivity.kt
+++ app/src/main/java/com/example/wheelim/LoginActivity.kt
@@ -4,20 +4,41 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
+import android.util.Log
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
 import com.example.wheelim.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 private lateinit var auth: FirebaseAuth
 
 class LoginActivity : AppCompatActivity(){
     private lateinit var binding: ActivityLoginBinding
     private var backPressedTime: Long = 0
+    private val TAG = this.javaClass.simpleName
+
+    private lateinit var launcher: ActivityResultLauncher<Intent>
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private var email: String = ""
+    private var tokenId: String? = null
     override fun onBackPressed() {
         if(System.currentTimeMillis() - backPressedTime >= 2000){
             backPressedTime = System.currentTimeMillis()
@@ -37,6 +58,54 @@ class LoginActivity : AppCompatActivity(){
         auth = Firebase.auth
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+        launcher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(), ActivityResultCallback { result ->
+                Log.e(TAG, "resultCode : ${result.resultCode}")
+                Log.e(TAG, "result : $result")
+                if(result.resultCode == RESULT_OK){
+                    val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                    try{
+                        task.getResult(ApiException::class.java)?.let{ account ->
+                            tokenId = account.idToken
+                            if(tokenId != null && tokenId != ""){
+                                val credential: AuthCredential = GoogleAuthProvider.getCredential(account.idToken, null)
+                                firebaseAuth.signInWithCredential(credential)
+                                    .addOnCompleteListener {
+                                        if(task.isSuccessful){
+                                            Log.e(TAG, "signInWithCredential: success")
+                                            val user = auth.currentUser
+                                            startActivity(Intent(this, MainActivity::class.java))
+                                            updateUI(user)
+                                        }else{
+                                            Log.w(TAG, "signInWithCredential: failure", task.exception)
+                                            updateUI(null)
+                                        }
+                                    }
+                            }else {
+                                Log.d(TAG, "No ID token!")
+                        }
+                        } ?: throw Exception()
+                    } catch (e: Exception){
+                        e.printStackTrace()
+                    }
+                }
+            })
+
+        binding.run{
+            googleLogin.setOnClickListener{
+                CoroutineScope(Dispatchers.IO).launch {
+                    val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestIdToken(getString(R.string.default_web_client_id))
+                        .requestEmail()
+                        .build()
+                    val googleSignInClient = GoogleSignIn.getClient(this@LoginActivity, gso)
+                    val signInIntent: Intent = googleSignInClient.signInIntent
+                    launcher.launch(signInIntent)
+                }
+            }
+        }
+
         binding.signup.setOnClickListener {
             val intent = Intent(this, JoinActivity::class.java)
             startActivity(intent)
@@ -52,17 +121,21 @@ class LoginActivity : AppCompatActivity(){
             val password = binding.loginPwd.text.toString()
             val intent = Intent(this, MainActivity::class.java)
 
-            if(email.isNotEmpty() && password.isNotEmpty())
+            if(email.isNotEmpty() && password.isNotEmpty()) {
                 auth.signInWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this) { task ->
-                        if(task.isSuccessful){
+                        if (task.isSuccessful) {
                             Toast.makeText(this, "로그인에 성공했습니다!", Toast.LENGTH_SHORT).show()
                             finishAffinity()
                             startActivity(intent)
-                        }else{
+                        } else {
                             Toast.makeText(this, "아이디와 비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show()
                         }
                     }
+            }
         }
+    }
+    private fun updateUI(user: FirebaseUser?){
+
     }
 }
--- app/src/main/java/com/example/wheelim/MainActivity.kt
+++ app/src/main/java/com/example/wheelim/MainActivity.kt
@@ -12,14 +12,25 @@ import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.example.wheelim.databinding.ActivityMainBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 private lateinit var auth: FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
     val mContext = this
+
+    private lateinit var nMap: GoogleMap
     private lateinit var binding: ActivityMainBinding
     private var backPressedTime: Long = 0
     override fun onBackPressed() {
@@ -30,7 +41,6 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
     }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -43,5 +53,23 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        binding.logout.setOnClickListener {
+            Firebase.auth.signOut()
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+      /*  val mapFragment = supportFragmentManager.findFragmentById(R.id.GPS_View) as SupportMapFragment
+        mapFragment.getMapAsync(this)*/
     }
+  /*  override fun onMapReady(googleMap: GoogleMap){
+        val seoul = LatLng(37.556, 126.97)
+
+        val markerOptions = MarkerOptions()
+        markerOptions.position(seoul)
+        markerOptions.title("서울")
+        markerOptions.snippet("한국 수도")
+
+        nMap?.addMarker(markerOptions)
+
+        nMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, 10f))
+    }*/
 }
